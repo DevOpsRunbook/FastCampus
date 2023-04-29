fb4491681f8aea2b95831ecf08136532ee7a16f8
Jun <shark2002@gmaiml.com>
2023-04-29 23:41:46 +0900

    refact: aws nexus container

--- Part2_Docker/Chapter06/nexus/nexus/start.sh
+++ Part2_Docker/Chapter06/nexus/nexus/start.sh
@@ -6,7 +6,7 @@ chmod go-rwx "$NEXUS_DATA/etc/ssl"
 keytool -genkeypair -keystore $NEXUS_DATA/etc/ssl/keystore.jks -storepass password -keypass password \
         -alias jetty -keyalg RSA -keysize 2048 -validity 5000 \
         -dname "CN=*.${HOSTNAME}, OU=FastCampus, O=FastCampus, L=Gangnam, ST=Seoul, C=KR" \
-        -ext "SAN=DNS:${SAN_DNS}" -ext "BC=ca:true"
+        -ext "SAN=DNS:ip-172-31-12-159.ap-northeast-2.compute.internal" -ext "BC=ca:true"
 fi
 
 sh -c ${SONATYPE_DIR}/start-nexus-repository-manager.sh
